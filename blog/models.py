f3f4bca944a76aa5c161716604d349d3cf7c3df9
Ivanir Efi <efi.ivanir@gmail.com>
2023-08-08 17:44:42 +0300

    Ch 01 until Building list and detail views

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+        .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -21,6 +26,8 @@ class Post(models.Model):
                               choices=Status.choices,
                               default=Status.DRAFT)
 
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
     class Meta:
         ordering = ['-publish']
         indexes = [
